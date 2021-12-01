0d2d7397547bf458d39c56c332813d0bb1af612b
Federico Jose Pacheco <fpacheco@fi.uba.ar>
2021-11-30 23:42:12 -0300

    'Scheme.clj' exportado para el main.

--- src/tp_lenguajes_formales_fiuba_pacheco_104541/core.clj
+++ src/tp_lenguajes_formales_fiuba_pacheco_104541/core.clj
@@ -1,9 +1,12 @@
-(ns tp-lenguajes-formales-fiuba-pacheco-104541.core
-  (:require [tp-lenguajes-formales-fiuba-pacheco-104541.probando_exports :refer :all]))
+(ns tp-lenguajes-formales-fiuba-pacheco-104541.core)
+  
+(require '[tp-lenguajes-formales-fiuba-pacheco-104541.probando_exports :refer :all]
+         '[tp-lenguajes-formales-fiuba-pacheco-104541.scheme :refer :all]
+)
 
 (declare ejemplo)
 (declare es-el-doble?)
-(declare spy)
+(declare spyy)
 
 (defn -main
   "Ejemplo de Proyecto en Clojure"
@@ -23,6 +26,6 @@
 (defn es-el-doble? [a b]
   (= (* 2 a) b))
 
-(defn spy
+(defn spyy
   ([x] (do (prn x) x))
   ([msg x] (do (print msg) (print ": ") (prn x) x)))
